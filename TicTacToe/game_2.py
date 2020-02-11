18a64515897302f64d06091e544ca96610799ae0
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2020-02-11 15:07:53 +0530

    Add files via upload

---
+++ TicTacToe/game_2.py
@@ -0,0 +1,65 @@
+import random
+
+combinations = [[1,2,3],[4,5,6],[7,8,9],
+                [1,4,7],[2,5,8],[3,6,9],
+                [1,5,9],[3,5,7]]
+
+occupied = []
+positions = [1,2,3,4,5,6,7,8,9]
+
+def gameBoard():
+    print("""
+      {}  |  {}  |  {}
+    -----|-----|------
+      {}  |  {}  |  {}  
+    -----|-----|------
+      {}  |  {}  |  {}
+    """.format(positions[0],positions[1],positions[2],
+                positions[3],positions[4],positions[5],
+                positions[6],positions[7],positions[8]))
+
+def userMoves(ch):
+    pos = int(input("Enter the position : "))
+    positions[pos - 1] = ch
+    occupied.append(pos - 1)
+    gameBoard()
+
+def cpuMoves(cpu_ch):
+    while True:
+        if len(occupied) < 8:
+            cpu_pos = random.randint(0, 8)
+            if cpu_pos in occupied:
+                print("Position Already Occupied...")
+            else:
+                print("CPU Moved at",cpu_pos+1)
+                positions[cpu_pos] = cpu_ch
+                occupied.append(cpu_pos)
+                break
+        else:
+            break
+    gameBoard()
+
+def checkWinner(pos,ch):
+    for i in range(len(combinations)):
+        if pos in combinations[i]:
+            index = combinations[i].index(pos)
+            combinations[i][index] = ch
+
+    for i in range(len(combinations)):
+        pass
+
+def main():
+    print("Welcome to Tic Tac Toe".center(40,"*"))
+    gameBoard()
+    ch = input("Please Enter a choice : X or 0 : ")
+    print("You have picked :",ch)
+    if ch == "X":
+        cpu_ch = 0
+    else: cpu_ch = "X"
+    while True:
+        userMoves(ch)
+        checkWinner()
+        cpuMoves(cpu_ch)
+        checkWinner()
+
+main()
